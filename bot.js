8432ed069e28854242f36532d19f74f0bba8bf00
STEAVESDF23 <46597603+STEAVESDF23@users.noreply.github.com>
2019-01-11 17:14:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -369,13 +369,13 @@ client.on('message', message => {
 });
 
 
-const devs = ["379687486906957824"];
-const adminprefix = ["-v"];
+const devs = ["406076958980571147"];
+const adminprefix = ["1"];
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
       
-  if (message.content.startsWith('(prefix))ply')) {
+  if (message.content.startsWith(adminprefix + "ply")) {
     client.user.setGame(argresult);
       message.channel.send(`**✅   ${argresult}**`)
   } else 
